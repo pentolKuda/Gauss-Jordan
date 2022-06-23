f788c494b24f506411657974e03b248bc15f721e
F_Zamzami <68177295+pentolKuda@users.noreply.github.com>
2022-06-23 14:56:07 +0800

    second commit

---
+++ Gauss-Jordan/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Gauss_Jordan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupEquation = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInputNumber = new System.Windows.Forms.TextBox();
+            this.groupEquation.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupEquation
+            // 
+            this.groupEquation.Controls.Add(this.dataGridView2);
+            this.groupEquation.Controls.Add(this.dataGridView1);
+            this.groupEquation.Controls.Add(this.button2);
+            this.groupEquation.Controls.Add(this.label6);
+            this.groupEquation.Controls.Add(this.label5);
+            this.groupEquation.Location = new System.Drawing.Point(40, 164);
+            this.groupEquation.Name = "groupEquation";
+            this.groupEquation.Size = new System.Drawing.Size(748, 533);
+            this.groupEquation.TabIndex = 10;
+            this.groupEquation.TabStop = false;
+            this.groupEquation.Text = "Equation";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(379, 68);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(347, 314);
+            this.dataGridView2.TabIndex = 17;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(316, 314);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 25);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Solve";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(186, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Matrix Size Equation:\r\n";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbInputNumber);
+            this.groupBox2.Location = new System.Drawing.Point(40, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(242, 118);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Equation Setup";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(121, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Ok";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input Number";
+            // 
+            // tbInputNumber
+            // 
+            this.tbInputNumber.Location = new System.Drawing.Point(121, 33);
+            this.tbInputNumber.Name = "tbInputNumber";
+            this.tbInputNumber.Size = new System.Drawing.Size(100, 22);
+            this.tbInputNumber.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 740);
+            this.Controls.Add(this.groupEquation);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Gauss-Jordan";
+            this.groupEquation.ResumeLayout(false);
+            this.groupEquation.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupEquation;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInputNumber;
+    }
+}
+
---
+++ Gauss-Jordan/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gauss_Jordan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int matrixSize;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                matrixSize = int.Parse(tbInputNumber.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Masukkan nilai dengan benar!");
+                return;
+            }
+            tbInputNumber.Text = "";
+
+            label6.Text = matrixSize.ToString() + " X " + matrixSize.ToString();
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+
+            //pertanyaan
+            for (int i = 0; i <= matrixSize; i++)
+            {
+                var column = new DataGridViewColumn();
+                column.HeaderText = (i == matrixSize ? "Y" : "X" + (i+1).ToString());
+                column.CellTemplate = new DataGridViewTextBoxCell();
+                column.Width = 30;
+                dataGridView1.Columns.Add(column);
+
+                if (i != matrixSize)
+                {
+                    dataGridView1.Rows.Add();
+                }
+            }
+
+            //jawaban
+            for (int i = 0; i < matrixSize; i++)
+            {
+                var colX1 = new DataGridViewColumn();
+                colX1.HeaderText = "X"+(i+1).ToString();
+                colX1.CellTemplate = new DataGridViewTextBoxCell();
+                colX1.Width = 70;
+                dataGridView2.Columns.Add(colX1);
+                
+            }
+            dataGridView2.Rows.Add();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[][] x_eq = new double[matrixSize][];
+            double[] y_eq = new double[matrixSize];
+            //double[] r_eq = new double[matrixSize];
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                x_eq[i] = new double[matrixSize+1];
+                for (int j = 0; j < matrixSize+1; j++)
+                {
+                    x_eq[i][j] = double.Parse((string)dataGridView1.Rows[i].Cells[j].Value);
+                    Console.Write(x_eq[i][j] + "\t");
+                }
+                
+                Console.WriteLine();
+            }
+
+            
+
+            for(int i=0; i< matrixSize; i++)
+            {
+                x_eq =  get_one(x_eq, i);
+                for(int j=0; j< matrixSize; j++)
+                {
+                    if(i != j)
+                    {
+                        x_eq =  get_zero(x_eq,j, i);
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for(int j=0; j< matrixSize+1; j++)
+                {
+                    Console.Write(x_eq[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                dataGridView2.Rows[0].Cells[i].Value = x_eq[i][matrixSize];
+            }
+
+        }
+
+
+        private double[][] get_one(double[][] mat, int pp)
+        {
+            int mat_size = mat[0].GetLength(0);
+
+            for(int i=0; i < mat_size; i++)
+            {
+                if(mat[pp][pp] != 1)
+                {
+                    double q00 = mat[pp][pp];
+                    for(int j = 0; j<mat_size; j++)
+                    {
+                        mat[pp][j] = mat[pp][j] / q00;
+                    }
+                }
+            }
+            return mat;
+        }
+
+        private double[][] get_zero(double[][] mat, int r, int c)
+        {
+            int mat_size = mat[0].GetLength(0);
+
+            for(int i=0; i<mat_size; i++)
+            {
+                if (mat[r][c] != 0)
+                {
+                    double q04 = mat[r][c];
+                    for(int j=0; j<mat_size; j++)
+                    {
+                        mat[r][j] = mat[r][j] - ((q04) * mat[c][j]);
+                    }
+                }
+            }
+
+            return mat;
+        }
+
+
+    }
+}
